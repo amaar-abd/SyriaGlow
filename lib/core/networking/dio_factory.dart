c3bb260d18e453cc13c7736eed9cd66f912a89a1
Amaar Abd <abdamaar64@gmail.com>
2026-05-26 02:07:28 +0300

    feat(network): implement DioFactory with BaseOptions and LogInterceptor for debugging

---
+++ lib/core/networking/dio_factory.dart
@@ -0,0 +1,35 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
+import 'package:syria_glow/core/networking/api_constants.dart';
+
+class DioFactory {
+  Dio? _dio;
+
+  Dio getDio() {
+    if (_dio == null) {
+      BaseOptions baseOptions = BaseOptions(
+        baseUrl: ApiConstants.baseUrl,
+        connectTimeout: const Duration(seconds: 30),
+        receiveTimeout: const Duration(seconds: 30),
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+        },
+      );
+
+      _dio = Dio(baseOptions);
+
+      if (_dio != null && kDebugMode) {
+        _dio!.interceptors.add(
+          LogInterceptor(
+            requestHeader: true,
+            requestBody: true,
+            responseBody: true,
+            error: true,
+          ),
+        );
+      }
+    }
+      return _dio!;
+  }
+}
