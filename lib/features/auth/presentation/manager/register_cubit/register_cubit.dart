8e9ac36893def9bb707ec0be63cb58255f4bc0b9
Amaar Abd <abdamaar64@gmail.com>
2026-05-26 02:11:56 +0300

    feat(auth): implement RegisterCubit with Initial, Loading, Success, and Error states

---
+++ lib/features/auth/presentation/manager/register_cubit/register_cubit.dart
@@ -0,0 +1,22 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:syria_glow/features/auth/data/models/auth_response.dart';
+import 'package:syria_glow/features/auth/data/models/register_request.dart';
+import 'package:syria_glow/features/auth/domain/user_cases/register_use_case.dart';
+part 'register_state.dart';
+
+class RegisterCubit extends Cubit<RegisterState> {
+  RegisterCubit(this.registerUseCase) : super(RegisterInitial());
+  final RegisterUseCase registerUseCase;
+  Future<void> registerAccount({
+    required RegisterRequest registerRequest,
+  }) async {
+    emit(RegisterLoading());
+
+    final result = await registerUseCase.call(registerRequest: registerRequest);
+
+    result.fold(
+      (failure) => emit(RegisterError(error: failure.message)),
+      (authResponse) => emit(RegisterSuccess(authResponse: authResponse)),
+    );
+  }
+}
