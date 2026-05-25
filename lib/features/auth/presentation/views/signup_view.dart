2061edc66b344049b06fad26676e2dca74928bbe
Amaar Abd <abdamaar64@gmail.com>
2026-05-26 02:12:08 +0300

    feat(auth): provide RegisterCubit above the SignUpView using BlocProvider

--- lib/features/auth/presentation/views/signup_view.dart
+++ lib/features/auth/presentation/views/signup_view.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:syria_glow/core/depandency_injection/service_locator.dart';
+import 'package:syria_glow/features/auth/domain/user_cases/register_use_case.dart';
+import 'package:syria_glow/features/auth/presentation/manager/register_cubit/register_cubit.dart';
 import 'package:syria_glow/features/auth/presentation/widgets/auth_appbar.dart';
 import 'package:syria_glow/features/auth/presentation/widgets/signup_view_body.dart';
 
@@ -8,7 +12,12 @@ class SignupView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(child: SignupViewBody()),
+      body: SafeArea(
+        child: BlocProvider(
+          create: (context) => RegisterCubit(sl.get<RegisterUseCase>()),
+          child: SignupViewBody(),
+        ),
+      ),
       appBar: authAppBar(context, showButton: true),
     );
   }
