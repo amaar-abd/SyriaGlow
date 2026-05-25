684ef1aeeca62ad736c3c1f4b863d059b85d9e8c
Amaar Abd <abdamaar64@gmail.com>
2026-05-26 02:07:41 +0300

    feat(network): create generic ApiService with dynamic GET and POST request methods

---
+++ lib/core/networking/api_service.dart
@@ -0,0 +1,43 @@
+import 'package:dio/dio.dart';
+
+class ApiService {
+  final Dio dio;
+
+  const ApiService({required this.dio});
+
+  Future<dynamic> get(
+    String path, {
+    Map<String, dynamic>? queryParameters,
+    Map<String, dynamic>? headers,
+  }) async {
+    try {
+      final response = await dio.get(
+        path,
+        queryParameters: queryParameters,
+        options: Options(headers: headers),
+      );
+      return response.data;
+    } on DioException catch (e) {
+      throw Exception(e.message);
+    }
+  }
+
+  Future<dynamic> post(
+    String path, {
+    Object? body,
+    Map<String, dynamic>? queryParameters,
+    Map<String, dynamic>? headers,
+  }) async {
+    try {
+      final response = await dio.post(
+        path,
+        data: body,
+        queryParameters: queryParameters,
+        options: Options(headers: headers),
+      );
+      return response.data;
+    } on DioException catch (e) {
+      throw Exception(e.message);
+    }
+  }
+}
