35894245b3cd1d5dac04d138c3746027e13ace72
Amaar Abd <abdamaar64@gmail.com>
2026-05-26 17:21:24 +0300

    feat(auth): add login method to AuthRemoteDataSource and its implementation

--- lib/features/auth/data/datasources/auth_remote_data_source.dart
+++ lib/features/auth/data/datasources/auth_remote_data_source.dart
@@ -1,8 +1,12 @@
 import 'package:syria_glow/features/auth/data/models/auth_response.dart';
+import 'package:syria_glow/features/auth/data/models/login_request.dart';
 import 'package:syria_glow/features/auth/data/models/register_request.dart';
 
 abstract class AuthRemoteDataSource {
   Future<AuthResponse> signUpWithEmailAndPassword(
     RegisterRequest registerRequest,
+  );
+  Future<AuthResponse> signInWithEmailAndPassword(
+    LoginRequest loginRequest,
   );
 }
--- lib/features/auth/data/datasources/auth_remote_data_source_impl.dart
+++ lib/features/auth/data/datasources/auth_remote_data_source_impl.dart
@@ -2,6 +2,7 @@ import 'package:syria_glow/core/networking/api_constants.dart';
 import 'package:syria_glow/core/networking/api_service.dart';
 import 'package:syria_glow/features/auth/data/datasources/auth_remote_data_source.dart';
 import 'package:syria_glow/features/auth/data/models/auth_response.dart';
+import 'package:syria_glow/features/auth/data/models/login_request.dart';
 import 'package:syria_glow/features/auth/data/models/register_request.dart';
 
 class AuthRemoteDataSourceImpl implements AuthRemoteDataSource {
@@ -12,12 +13,21 @@ class AuthRemoteDataSourceImpl implements AuthRemoteDataSource {
   Future<AuthResponse> signUpWithEmailAndPassword(
     RegisterRequest registerRequest,
   ) async {
+    final response = await apiService.post(
+      ApiConstants.register,
+      body: registerRequest.toJson(),
+    );
+    return AuthResponse.fromJson(response);
+  }
 
-      final response = await apiService.post(
-        ApiConstants.register,
-        body: registerRequest.toJson(),
-      );
-      return AuthResponse.fromJson(response);
-   
+  @override
+  Future<AuthResponse> signInWithEmailAndPassword(
+    LoginRequest loginRequest,
+  ) async {
+    final response = await apiService.post(
+      ApiConstants.login,
+      queryParameters: loginRequest.toJson(),
+    );
+    return AuthResponse.fromJson(response);
   }
 }
