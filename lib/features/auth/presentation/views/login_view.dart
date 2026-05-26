d766a0a6a38171d4509a1763f1bbf5054a67a846
Amaar Abd <abdamaar64@gmail.com>
2026-05-26 17:23:35 +0300

    feat(auth_ui): provide LoginCubit above the login screen using BlocProvider

--- lib/features/auth/presentation/views/login_view.dart
+++ lib/features/auth/presentation/views/login_view.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:syria_glow/core/depandency_injection/service_locator.dart';
+import 'package:syria_glow/features/auth/domain/user_cases/login_use_case.dart';
+import 'package:syria_glow/features/auth/presentation/manager/login_cubit/login_cubit.dart';
 import 'package:syria_glow/features/auth/presentation/widgets/auth_appbar.dart';
 import 'package:syria_glow/features/auth/presentation/widgets/login_view_body.dart';
 
@@ -8,7 +12,12 @@ class LoginView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(child: LoginViewBody()),
+      body: SafeArea(
+        child: BlocProvider(
+          create: (context) => LoginCubit(sl.get<LoginUseCase>()),
+          child: LoginViewBody(),
+        ),
+      ),
       appBar: authAppBar(context),
     );
   }
