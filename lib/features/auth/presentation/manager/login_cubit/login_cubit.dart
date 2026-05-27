9f0576833f596d6b38a46aba3de5b8f257615500
Amaar Abd <abdamaar64@gmail.com>
2026-05-28 02:23:09 +0300

    feat(auth): create logout use case

--- lib/features/auth/presentation/manager/login_cubit/login_cubit.dart
+++ lib/features/auth/presentation/manager/login_cubit/login_cubit.dart
@@ -1,7 +1,7 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:syria_glow/features/auth/data/models/auth_response.dart';
 import 'package:syria_glow/features/auth/data/models/login_request.dart';
-import 'package:syria_glow/features/auth/domain/user_cases/login_use_case.dart';
+import 'package:syria_glow/features/auth/domain/use_cases/login_use_case.dart';
 
 part 'login_state.dart';
 
--- lib/features/auth/presentation/manager/register_cubit/register_cubit.dart
+++ lib/features/auth/presentation/manager/register_cubit/register_cubit.dart
@@ -1,7 +1,7 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:syria_glow/features/auth/data/models/auth_response.dart';
 import 'package:syria_glow/features/auth/data/models/register_request.dart';
-import 'package:syria_glow/features/auth/domain/user_cases/register_use_case.dart';
+import 'package:syria_glow/features/auth/domain/use_cases/register_use_case.dart';
 part 'register_state.dart';
 
 class RegisterCubit extends Cubit<RegisterState> {
