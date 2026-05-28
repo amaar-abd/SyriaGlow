747debe6b90502086c886f51f4055029a8fc1d0e
Amaar Abd <abdamaar64@gmail.com>
2026-05-28 14:36:47 +0300

    feat(navigation): add password recovery routes to generateRoute

--- lib/core/routes/app_routes.dart
+++ lib/core/routes/app_routes.dart
@@ -3,4 +3,9 @@ class AppRoutes {
   static const String onboardingView = 'OnboardingView';
   static const String loginView = 'LoginView';
   static const String signupView = 'SignupView';
+  static const String mainLayoutView = 'MainLayoutView';
+  static const String forgotePasswordView = 'ForgotePasswordView';
+  static const String otpVerificationView = 'OtpVerificationView';
+  static const String resetPasswordView = 'ResetPasswordView';
+  static const String successResetPasswordViw = 'SuccessResetPasswordViw';
 }
--- lib/core/routes/route_generator.dart
+++ lib/core/routes/route_generator.dart
@@ -1,7 +1,13 @@
+
 import 'package:flutter/material.dart';
 import 'package:syria_glow/core/routes/app_routes.dart';
+import 'package:syria_glow/features/auth/presentation/views/forgote_password/forgote_password_view.dart';
+import 'package:syria_glow/features/auth/presentation/views/forgote_password/otp_verification_view.dart';
+import 'package:syria_glow/features/auth/presentation/views/forgote_password/reset_password_view.dart';
+import 'package:syria_glow/features/auth/presentation/views/forgote_password/success_reset_password_viw.dart';
 import 'package:syria_glow/features/auth/presentation/views/login_view.dart';
 import 'package:syria_glow/features/auth/presentation/views/signup_view.dart';
+import 'package:syria_glow/features/main_layout/presentation/views/main_layout_view.dart';
 import 'package:syria_glow/features/onboarding/presentation/views/onboarding_view.dart';
 import 'package:syria_glow/features/splash/presentation/views/splash_view.dart';
 
@@ -9,13 +15,32 @@ class RouteGenerator {
   static Route<dynamic> onGenerateRoute(RouteSettings settings) {
     switch (settings.name) {
       case AppRoutes.splashView:
-        return MaterialPageRoute(builder: (context) =>const SplashView());
+        return MaterialPageRoute(builder: (context) => const SplashView());
       case AppRoutes.onboardingView:
-        return MaterialPageRoute(builder: (context) =>const OnboardingView());
+        return MaterialPageRoute(builder: (context) => const OnboardingView());
       case AppRoutes.loginView:
-        return MaterialPageRoute(builder: (context) =>const LoginView());
+        return MaterialPageRoute(builder: (context) => const LoginView());
       case AppRoutes.signupView:
-        return MaterialPageRoute(builder: (context) =>const SignupView());
+        return MaterialPageRoute(builder: (context) => const SignupView());
+      case AppRoutes.mainLayoutView:
+        return MaterialPageRoute(builder: (context) => const MainLayoutView());
+      case AppRoutes.forgotePasswordView:
+        return MaterialPageRoute(
+          builder: (context) => const ForgotePasswordView(),
+        );
+      case AppRoutes.otpVerificationView:
+        final  email = settings.arguments ;
+        return MaterialPageRoute(
+          builder: (context) => OtpVerificationView(email: email as String),
+        );
+      case AppRoutes.resetPasswordView:
+        return MaterialPageRoute(
+          builder: (context) => const ResetPasswordView(),
+        );
+      case AppRoutes.successResetPasswordViw:
+        return MaterialPageRoute(
+          builder: (context) => const SuccessResetPasswordViw(),
+        );
 
       default:
         return MaterialPageRoute(
