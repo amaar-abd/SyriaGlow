de80d02587c2dd7badb030ca5ed3b1218d792921
Amaar Abd <abdamaar64@gmail.com>
2026-06-01 19:26:11 +0300

    feat: implement saveAuthToken upon successful registration using SecureStorageService

--- lib/features/auth/presentation/widgets/login_bloc_consumer.dart
+++ lib/features/auth/presentation/widgets/login_bloc_consumer.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:syria_glow/core/constants/app_constatntes.dart';
+import 'package:syria_glow/core/depandency_injection/service_locator.dart';
 import 'package:syria_glow/core/extensions/context_extensions.dart';
 import 'package:syria_glow/core/routes/app_routes.dart';
+import 'package:syria_glow/core/services/secure_storage_service.dart';
 import 'package:syria_glow/core/theme/app_colors.dart';
 import 'package:syria_glow/core/utils/custom_snackbar.dart';
 import 'package:syria_glow/core/utils/main_button.dart';
@@ -25,21 +28,30 @@ class LoginBlocConsumer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocConsumer<LoginCubit, LoginState>(
-      listener: (context, state) {
+      listener: (context, state) async {
         if (state is LoginSuccess) {
           customSnackBar(
             context,
             state.authResponse.message,
             AppColors.primaryGreenLight,
           );
-          Navigator.pushNamedAndRemoveUntil(
-            context,
-            AppRoutes.mainLayoutView,
-            (route) => false,
+          await sl<SecureStorageService>().write(
+            AppConstants.authToken,
+            state.authResponse.token,
           );
+
+          if (context.mounted) {
+            Navigator.pushNamedAndRemoveUntil(
+              context,
+              AppRoutes.mainLayoutView,
+              (route) => false,
+            );
+          }
         }
         if (state is LoginError) {
-          customSnackBar(context, state.error, AppColors.error);
+          if (context.mounted) {
+            customSnackBar(context, state.error, AppColors.error);
+          }
         }
       },
       builder: (context, state) {
