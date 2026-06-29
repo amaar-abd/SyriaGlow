7afe63f47f6a0477e08da06f6abaf27aa8bfe613
Amaar Abd <abdamaar64@gmail.com>
2026-06-29 12:13:48 +0300

    add routePoints, distance, and duration to UserLocationSuccess state

--- lib/features/home/presentation/manager/user_location_cubit/user_location_state.dart
+++ lib/features/home/presentation/manager/user_location_cubit/user_location_state.dart
@@ -1,3 +1,6 @@
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
 sealed class UserLocationState {}
 
 final class UserLocationInitial extends UserLocationState {}
@@ -6,8 +9,36 @@ final class UserLocationLoading extends UserLocationState {}
 
 final class UserLocationSuccess extends UserLocationState {
   final String address;
-
-  UserLocationSuccess({required this.address});
+  final Position position;
+  final List<LatLng> routePoints;
+  final String distance;
+  final String duration;
+  final bool isRouteLoading;
+  UserLocationSuccess({
+    this.routePoints = const [],
+    this.distance = '',
+    this.duration = '',
+    this.isRouteLoading = false,
+    required this.address,
+    required this.position,
+  });
+  UserLocationSuccess copyWith({
+    String? address,
+    Position? position,
+    List<LatLng>? routePoints,
+    String? distance,
+    String? duration,
+    bool? isRouteLoading,
+  }) {
+    return UserLocationSuccess(
+      address: address ?? this.address,
+      position: position ?? this.position,
+      routePoints: routePoints ?? this.routePoints,
+      distance: distance ?? this.distance,
+      duration: duration ?? this.duration,
+      isRouteLoading: isRouteLoading ?? this.isRouteLoading,
+    );
+  }
 }
 
 final class UserLocationFailure extends UserLocationState {
