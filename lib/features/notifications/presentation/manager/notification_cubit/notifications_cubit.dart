e35bad2ebeae36a49e7b7ec9fa630e4611ad6d56
Amaar Abd <abdamaar64@gmail.com>
2026-07-03 00:18:52 +0300

    feat(cubit): add streamNotifications and clearRedDot methods

---
+++ lib/features/notifications/presentation/manager/notification_cubit/notifications_cubit.dart
@@ -0,0 +1,48 @@
+import 'dart:async';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:syria_glow/features/notifications/data/models/notification_model.dart';
+import 'package:syria_glow/features/notifications/domain/repos/notifications_repository.dart';
+
+part 'notifications_state.dart';
+
+class NotificationsCubit extends Cubit<NotificationsState> {
+  NotificationsCubit(this.repository) : super(NotificationsInitial());
+
+  final NotificationsRepository repository;
+  StreamSubscription? _notificationsSubscription;
+
+  void streamNotifications() {
+    emit(NotificationsLoading());
+    _notificationsSubscription?.cancel();
+
+    _notificationsSubscription = repository.streamNotifications().listen(
+      (notifications) {
+        bool showRedDot = notifications.any(
+          (notification) => !notification.isRead,
+        );
+        emit(
+          NotificationsSuccess(
+            notifications: notifications,
+            unread: showRedDot,
+          ),
+        );
+      },
+      onError: (error) {
+        emit(NotificationsFailure(message: error.toString()));
+      },
+    );
+  }
+
+  void clearRedDot() {
+    if (state is NotificationsSuccess) {
+      final currentList = (state as NotificationsSuccess).notifications;
+      emit(NotificationsSuccess(notifications: currentList, unread: false));
+    }
+  }
+
+  @override
+  Future<void> close() {
+    _notificationsSubscription?.cancel();
+    return super.close();
+  }
+}
