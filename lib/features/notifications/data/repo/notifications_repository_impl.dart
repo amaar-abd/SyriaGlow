64fa824996a4a4f1d65e2c64f0d8142759e8f2a4
Amaar Abd <abdamaar64@gmail.com>
2026-07-03 00:10:20 +0300

    eat(notifications): add remote data source and repository for streaming notifications

---
+++ lib/features/notifications/data/repo/notifications_repository_impl.dart
@@ -0,0 +1,15 @@
+import 'package:syria_glow/features/notifications/data/data_source/notifications_remote_data_source.dart';
+import 'package:syria_glow/features/notifications/data/models/notification_model.dart';
+import 'package:syria_glow/features/notifications/domain/repos/notifications_repository.dart';
+
+class NotificationsRepositoryImpl implements NotificationsRepository {
+  NotificationsRepositoryImpl({required this.remoteDataSource});
+  final NotificationsRemoteDataSource remoteDataSource;
+
+  @override
+  Stream<List<NotificationModel>> streamNotifications() {
+    return remoteDataSource.getNotificationsStream().map((snapshot) {
+      return snapshot.docs.map((doc) => NotificationModel.fromFirestore(doc)).toList();
+    });
+  }
+}
---
+++ lib/features/notifications/domain/repos/notifications_repository.dart
@@ -0,0 +1,5 @@
+import 'package:syria_glow/features/notifications/data/models/notification_model.dart';
+
+abstract class NotificationsRepository {
+  Stream<List<NotificationModel>> streamNotifications();
+}
