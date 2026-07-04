d0b19872d67177f48d74303e13355993bedf9443
Amaar Abd <abdamaar64@gmail.com>
2026-07-05 00:40:46 +0300

    feat(explore): add search  and resetSearch methods to ExploreCubit

---
+++ lib/features/explore/presentation/manager/explore_cubit/explore_cubit.dart
@@ -0,0 +1,44 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:syria_glow/features/explore/domain/repo/explore_repository.dart';
+import 'package:syria_glow/features/explore/presentation/manager/explore_cubit/explore_state.dart';
+
+class ExploreCubit extends Cubit<ExploreState> {
+  ExploreCubit(this.repository) : super(ExploreState());
+  final ExploreRepository repository;
+
+  Future<void> search({required String query}) async {
+    emit(state.copyWith(searchStatus: SearchStatus.loading));
+    final results = await repository.searchByNmae(query: query);
+    results.fold(
+      (failure) {
+        emit(
+          state.copyWith(
+            searchStatus: SearchStatus.failure,
+            searchErrorMessage: failure.message,
+          ),
+        );
+      },
+      (landmarks) {
+        if (landmarks.isEmpty) {
+          emit(state.copyWith(searchStatus: SearchStatus.noResults));
+        } else {
+          emit(
+            state.copyWith(
+              searchStatus: SearchStatus.success,
+              searchResults: landmarks,
+            ),
+          );
+        }
+      },
+    );
+  }
+
+  void resetSearch() {
+    emit(
+      state.copyWith(
+        searchStatus: SearchStatus.initial,
+        searchResults: const [],
+      ),
+    );
+  }
+}
