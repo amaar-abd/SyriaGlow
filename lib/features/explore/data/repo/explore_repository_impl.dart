4299de1efbc105a20ad29f52a7a9db9185d04f45
Amaar Abd <abdamaar64@gmail.com>
2026-07-05 00:39:17 +0300

    feat(explore): add search method to repository and its implementation

---
+++ lib/features/explore/data/repo/explore_repository_impl.dart
@@ -0,0 +1,23 @@
+import 'package:dartz/dartz.dart';
+import 'package:syria_glow/core/errors/dio_error_handler.dart';
+import 'package:syria_glow/core/errors/failure.dart';
+import 'package:syria_glow/features/explore/data/data_source/explore_remote_data_source.dart';
+import 'package:syria_glow/features/explore/domain/repo/explore_repository.dart';
+import 'package:syria_glow/features/home/data/models/landmark_model.dart';
+
+class ExploreRepositoryImpl implements ExploreRepository {
+  ExploreRepositoryImpl({required this.exploreRemoteDataSource});
+  final ExploreRemoteDataSource exploreRemoteDataSource;
+
+  @override
+  Future<Either<Failure, List<Landmark>>> searchByNmae({
+    required String query,
+  }) async {
+    try {
+      final response = await exploreRemoteDataSource.searchByNmae(query: query);
+      return right(response.landmarks);
+    } catch (e) {
+       return left(DioErrorHandler.handle(e));
+    }
+  }
+}
---
+++ lib/features/explore/domain/repo/explore_repository.dart
@@ -0,0 +1,7 @@
+import 'package:dartz/dartz.dart';
+import 'package:syria_glow/core/errors/failure.dart';
+import 'package:syria_glow/features/home/data/models/landmark_model.dart';
+
+abstract class ExploreRepository {
+   Future<Either<Failure,List<Landmark>>> searchByNmae({required String query});
+}
