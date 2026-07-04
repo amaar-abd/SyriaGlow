9bfd33b1551593780189a41b037f9d440766fedc
Amaar Abd <abdamaar64@gmail.com>
2026-07-05 00:46:13 +0300

    explore ui: handle search success state and build results list view

---
+++ lib/features/explore/presentation/widgets/search_results_item.dart
@@ -0,0 +1,102 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:syria_glow/core/theme/app_colors.dart';
+import 'package:syria_glow/features/home/data/models/landmark_model.dart';
+
+class SearchResultsItem extends StatelessWidget {
+  const SearchResultsItem({
+    super.key,
+    required this.landmark,
+    required this.onTap,
+  });
+  final Landmark landmark;
+  final void Function()? onTap;
+  @override
+  Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+    final imageUrl = landmark.images.isNotEmpty
+        ? landmark.images.first.image
+        : '';
+    return InkWell(
+      onTap: onTap,
+      child: Container(
+        margin: EdgeInsets.only(left: 16.w, right: 16.w,top: 12.h),
+        padding: EdgeInsets.all(10.r),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(12.r),
+          border: Border.all(color: AppColors.primaryGreen.withAlpha(10)),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withAlpha(40),
+              blurRadius: 6,
+              offset: const Offset(0, 2),
+            ),
+          ],
+        ),
+        child: Row(
+          children: [
+            Container(
+              width: 70.w,
+              height: 70.h,
+              decoration: BoxDecoration(
+                color: AppColors.primaryGreen,
+                borderRadius: BorderRadius.circular(16.r),
+              ),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(16.r),
+                child: CachedNetworkImage(
+                  imageUrl: imageUrl,
+                  fit: BoxFit.cover,
+                  placeholder: (context, url) => const Center(
+                    child: CircularProgressIndicator(
+                      color: Color(0xFFD4AF37),
+                      strokeWidth: 3,
+                    ),
+                  ),
+                  errorWidget: (context, url, error) =>
+                      const Icon(Icons.broken_image, size: 50),
+                ),
+              ),
+            ),
+            SizedBox(width: 12.w),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    landmark.nameAr,
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                    style: textTheme.bodyMedium?.copyWith(
+                      color: AppColors.primaryGreen,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 4.h),
+                  Text(
+                    landmark.address,
+                         maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                    style: textTheme.bodySmall?.copyWith(
+                      color: AppColors.elegantGold,
+                      fontSize: 10.sp,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+            FaIcon(
+              FontAwesomeIcons.chevronLeft,
+              color: AppColors.primaryGreen.withAlpha(200),
+              size: 14.r,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/features/explore/presentation/widgets/search_results_list_view.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:syria_glow/core/routes/app_routes.dart';
+import 'package:syria_glow/features/explore/presentation/widgets/search_results_item.dart';
+import 'package:syria_glow/features/home/data/models/landmark_model.dart';
+
+class SearchResultsListView extends StatelessWidget {
+  const SearchResultsListView({super.key, required this.landmarks});
+  final List<Landmark> landmarks;
+  @override
+  Widget build(BuildContext context) {
+    return SliverList.builder(
+      itemCount: landmarks.length,
+
+      itemBuilder: (context, index) {
+        return SearchResultsItem(
+          landmark: landmarks[index],
+          onTap: () {
+            Navigator.of(
+              context,
+            ).pushNamed(AppRoutes.homeDetailsView, arguments: landmarks[index]);
+          },
+        );
+      },
+    );
+  }
+}
