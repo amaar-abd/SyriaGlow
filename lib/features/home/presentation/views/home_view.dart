14a515e2ffa2b6462f4a3ae0ce47ea8995a26d0c
Amaar Abd <abdamaar64@gmail.com>
2026-07-14 02:11:58 +0300

    Wrap Home AppBar profile image with BlocBuilder to listen for state changes

--- lib/features/home/presentation/views/home_view.dart
+++ lib/features/home/presentation/views/home_view.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -5,20 +6,19 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:syria_glow/core/depandency_injection/service_locator.dart';
 import 'package:syria_glow/core/routes/app_routes.dart';
 import 'package:syria_glow/core/theme/app_colors.dart';
-import 'package:syria_glow/core/utils/app_images.dart';
 import 'package:syria_glow/features/home/presentation/manager/home_cubit/home_cubit.dart';
 import 'package:syria_glow/features/home/presentation/widgets/home_widgets/home_view_body.dart';
 import 'package:syria_glow/features/main_layout/presentation/widgets/location_info_widget.dart';
 import 'package:syria_glow/features/notifications/presentation/manager/notification_cubit/notifications_cubit.dart';
+import 'package:syria_glow/features/profile/presentation/manager/profile_cubit/profile_cubit.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    
     return Scaffold(
-    appBar:  AppBar(
+      appBar: AppBar(
         automaticallyImplyLeading: false,
         backgroundColor: Colors.white,
         scrolledUnderElevation: 0,
@@ -26,7 +26,73 @@ class HomeView extends StatelessWidget {
           mainAxisSize: MainAxisSize.min,
           mainAxisAlignment: MainAxisAlignment.start,
           children: [
-            Image.asset(Assets.assetsImagesMan, width: 40.r),
+            BlocBuilder<ProfileCubit, ProfileState>(
+              builder: (context, state) {
+                bool isUploading = state is ProfileImageUploading;
+                if (state is ProfileSuccess) {
+                  return Container(
+                    padding: EdgeInsets.all(2.w),
+                    decoration: BoxDecoration(
+                      color: AppColors.elegantGold,
+                      borderRadius: BorderRadius.circular(35.r),
+                    ),
+                    child: CircleAvatar(
+                      radius: 20.r,
+                      backgroundColor: AppColors.surfaceWhite,
+                      child: isUploading
+                          ? Padding(
+                              padding: EdgeInsets.all(8.r),
+                              child: CircularProgressIndicator(
+                                color: AppColors.primaryGreen,
+                                strokeWidth: 3.w,
+                              ),
+                            )
+                          : state.profile.imageUrl != null
+                          ? ClipOval(
+                              child: CachedNetworkImage(
+                                imageUrl: state.profile.imageUrl!,
+                                width: 40.w,
+                                height: 40.h,
+                                fit: BoxFit.cover,
+                                placeholder: (context, url) => CircleAvatar(
+                                  radius: 20.r,
+                                  backgroundColor: AppColors.surfaceWhite,
+                                  child: ClipOval(
+                                    child: CircularProgressIndicator(
+                                      color: AppColors.primaryGreen,
+                                      strokeWidth: 3.w,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            )
+                          : DecoratedBox(
+                            decoration: BoxDecoration(
+                color: AppColors.primaryGreen.withAlpha(20),
+                borderRadius: BorderRadius.circular(30),
+              ),
+                            child: Icon(
+                                Icons.person,
+                                size: 22.r,
+                                color: AppColors.primaryGreen,
+                              ),
+                          ),
+                    ),
+                  );
+                }
+                return DecoratedBox(
+                  decoration: BoxDecoration(
+                color: AppColors.primaryGreen.withAlpha(20),
+                borderRadius: BorderRadius.circular(30),
+              ),
+                  child: Icon(
+                    Icons.person,
+                    size: 22.r,
+                    color: AppColors.primaryGreen,
+                  ),
+                );
+              },
+            ),
             SizedBox(width: 10.w),
             DecoratedBox(
               decoration: BoxDecoration(
@@ -77,13 +143,13 @@ class HomeView extends StatelessWidget {
         actions: [
           Padding(
             padding: EdgeInsets.symmetric(horizontal: 2.r),
-            child: Center(child: LocationInfoWidget()),
+            child: Center(child: const LocationInfoWidget()),
           ),
         ],
       ),
       body: BlocProvider(
         create: (context) => sl.get<HomeCubit>()..mostVisitedPlaces(),
-        child:const HomeViewBody(),
+        child: const HomeViewBody(),
       ),
     );
   }
