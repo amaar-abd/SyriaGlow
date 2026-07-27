96223b5a23babde32c56fcbd1622d8e7505305d2
Amaar Abd <abdamaar64@gmail.com>
2026-07-27 23:11:19 +0300

    (build.gradel.kts): add isMinifyEnabled ,isShrinkResources with values

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -28,6 +28,8 @@ android {
 
     buildTypes {
         release {
+            isMinifyEnabled = true
+            isShrinkResources = true
             signingConfig = signingConfigs.getByName("debug")
         }
     }
